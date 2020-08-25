46134b1d0bf256b000cb0b12225381056b5ec1d5
Derek Rush <drush81@gmail.com>
2020-08-25 14:58:54 -0700

    release version

--- src/App.js
+++ src/App.js
@@ -7,19 +7,20 @@ function App() {
   return (
     <div className="App">
       <header className="header">
-        <img src={iilogo} className="App-logo" alt="Insightful Innovations logo" />
+        <img src={iilogo} className="mx-auto mb-4" alt="Insightful Innovations logo" />
         <p className="desc">
-          We build highly available online tools to help teams and organizations manage marketing and sales. We do this through automation, and machine learning to maximize efficiency and ROI.
+          We build highly available online tools to help teams and organizations manage marketing and sales. We do this through automation and machine learning to maximize efficiency and ROI.
         </p>
       </header>
       <h2 className="sub-header">Products</h2>
       <div className="products">
-        <img src={excytelogo} className="App-logo" alt="Excyte logo" />
+        <img src={excytelogo} className="mx-auto" alt="Excyte logo" />
       </div>
       <h2 className="sub-header">Contact</h2>
       <div className="products">
         <a className="link" href="mailto:derek@insightful-innovations.com">contact@insightful-innovations.com</a>
       </div>
+      <p className="copy mt-8">&#169; Insightful Innovations</p>
     </div>
   );
 }
